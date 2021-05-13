118c8d16c311b6fc602d422c6ba3024084ae086f
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2021-05-13 19:53:38 -0400

    Update logger.go

--- gosnow/logger.go
+++ gosnow/logger.go
@@ -2,16 +2,24 @@ package gosnow
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 var (
-	logger *log.Logger
+	logger   *log.Logger
+	disabled = false
 )
 
 func init() {
 	logger = log.New(os.Stdout, "logger: ", log.Lshortfile)
+}
+
+func DisableLogger() {
+	disabled = true
+	logger.SetFlags(0)
+	SetLogOutput(ioutil.Discard)
 }
 
 func SetLogOutput(writer io.Writer) {
