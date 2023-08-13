4a3d67abfd2e7f87371afbbef8394bf70f6e7174
michaeldcanady <michaeldcanady+github@gmail.com>
2023-08-13 14:23:28 -0400

    beginning

---
+++ request_builder.go
@@ -0,0 +1,13 @@
+package gosnow
+
+type RequestBuilder struct {
+	Url    string
+	Client *Client
+}
+
+func NewRequestBuilder(url string, client *Client) *RequestBuilder {
+	return &RequestBuilder{
+		Url:    url,
+		Client: client,
+	}
+}
